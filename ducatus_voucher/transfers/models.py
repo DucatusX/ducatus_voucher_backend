c15ff004308a922cbcab5b6d88a55c602ea0b36f
Egor14 <egorgrekhnev@mail.ru>
2020-06-26 15:54:48 +0300

    foreign key instead one_to_one

--- ducatus_voucher/transfers/models.py
+++ ducatus_voucher/transfers/models.py
@@ -5,7 +5,7 @@ from ducatus_voucher.consts import MAX_DIGITS
 
 
 class Transfer(models.Model):
-    voucher = models.OneToOneField(Voucher, on_delete=models.CASCADE)
+    voucher = models.ForeignKey(Voucher, on_delete=models.CASCADE)
     duc_amount = models.DecimalField(max_digits=MAX_DIGITS, decimal_places=0)
     tx_hash = models.CharField(max_length=100, null=True, default=None)
     duc_address = models.CharField(max_length=50)
