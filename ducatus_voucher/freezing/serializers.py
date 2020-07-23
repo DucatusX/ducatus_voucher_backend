2c8e2444293fd3c8ec26946969ad41b7119e7609
Egor14 <egorgrekhnev@mail.ru>
2020-07-23 12:12:07 +0300

    duc and usd amount in frozen vouchers vieews

--- ducatus_voucher/freezing/serializers.py
+++ ducatus_voucher/freezing/serializers.py
@@ -1,9 +1,15 @@
 from rest_framework import serializers
 
 from ducatus_voucher.freezing.models import FreezingVoucher
+from ducatus_voucher.consts import DECIMALS
 
 
 class FreezingVoucherSerializer(serializers.ModelSerializer):
     class Meta:
         model = FreezingVoucher
         fields = '__all__'
+
+    def to_representation(self, instance):
+        res = super().to_representation(instance)
+        res['duc_amount'] = int(instance.voucher.transfer_set.first().duc_amount) // DECIMALS['DUC']
+        res['usd_amount'] = instance.voucher.usd_amount
