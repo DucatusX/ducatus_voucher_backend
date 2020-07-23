c27b9abb7063d4d96a6559f5587ff459846ac5be
Egor14 <egorgrekhnev@mail.ru>
2020-07-23 19:29:53 +0300

    withdraw checker logs

--- withdraw_checker.py
+++ withdraw_checker.py
@@ -2,6 +2,7 @@ import os
 import sys
 import time
 import traceback
+import datetime
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ducatus_voucher.settings')
 import django
@@ -14,12 +15,11 @@ from ducatus_voucher.litecoin_rpc import DucatuscoreInterface
 from ducatus_voucher.freezing.models import FreezingVoucher
 from ducatus_voucher.settings import WITHDRAW_CHECKER_TIMEOUT
 
-
 if __name__ == '__main__':
     while True:
         frozen_vouchers = FreezingVoucher.objects.filter(withdrawn=False, lock_time__lt=timezone.now().timestamp())
         if not frozen_vouchers:
-            print('all frozen vouchers have withdrawn\n', flush=True)
+            print('all frozen vouchers have withdrawn at {}\n'.format(datetime.datetime.now()), flush=True)
 
         interface = DucatuscoreInterface()
         for frozen_voucher in frozen_vouchers:
@@ -32,9 +32,13 @@ if __name__ == '__main__':
                     if not unspent_tx:
                         frozen_voucher.withdrawn = True
                         frozen_voucher.save()
-                        print('voucher with id {id} withdrawn\n'.format(id=frozen_voucher.id), flush=True)
+                        print('voucher with id {id} withdrawn at {time}\n'.format(id=frozen_voucher.id,
+                                                                                  time=datetime.datetime.now()),
+                              flush=True)
                     else:
-                        print('voucher with id {id} not withdrawn yet\n'.format(id=frozen_voucher.id), flush=True)
+                        print('voucher with id {id} not withdrawn yet at {time}\n'.format(id=frozen_voucher.id,
+                                                                                          time=datetime.datetime.now()),
+                              flush=True)
             except Exception as e:
                 print('\n'.join(traceback.format_exception(*sys.exc_info())), flush=True)
 
