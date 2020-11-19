b6c2d97e10f4adc76dc348a0ea1f062734f42c92
Euphemia Deathridge <ephdtrg@mintyclouds.in>
2020-11-19 14:37:09 +0300

    function to get staking statistics

---
+++ ducatus_voucher/staking/api.py
@@ -0,0 +1,23 @@
+import os
+import csv
+from django.utils import timezone
+from ducatus_voucher.staking.models import DepositInput
+
+
+def export_deposit_statistics():
+    unspent_dividends_inputs = DepositInput.objects.filter(spent_tx_hash=None, deposit__dividends__gt=0)
+
+    if not unspent_dividends_inputs:
+        print('All deposits has been withdrawn, nothing to export', flush=True)
+
+    stat_fn = f'staking-{timezone.now().date()}.csv'
+    with open(os.getcwd(), stat_fn, 'w') as csvfile:
+        writer = csv.writer(csvfile)
+        for deposit_input in unspent_dividends_inputs:
+            writer.writerow(
+                [
+                    deposit_input.mint_tx_hash,
+                    deposit_input.amount,
+                    deposit_input.deposit.cltv_details.lock_time
+                ]
+            )
