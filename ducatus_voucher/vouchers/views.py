2ec616d46317f7a4600ab3bdebe0a93a0e53cc99
Euphemia Deathridge <ephdtrg@mintyclouds.in>
2021-03-05 05:44:53 +0300

    add decorator

--- ducatus_voucher/vouchers/views.py
+++ ducatus_voucher/vouchers/views.py
@@ -7,7 +7,7 @@ from drf_yasg.utils import swagger_auto_schema
 from rest_framework.response import Response
 from rest_framework.request import Request
 from rest_framework.exceptions import ValidationError
-from rest_framework.decorators import api_view
+from rest_framework.decorators import api_view, authentication_classes
 from rest_framework.exceptions import NotFound
 from rest_framework.exceptions import PermissionDenied
 
